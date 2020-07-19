6fd59b9e3fb842781d37b9ac0b258153f8027b6f
Greg Hawthorne <ghawthorne@me.com>
2020-07-19 05:28:32 -0700

    first commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="header">
+        <h1>ToDoList</h1>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    #nav {
+        color: #42b983;
+    }
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+        text-decoration: none;
+    }
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.1.0 it is made with vue.js</p>
+  </div>
+</template>
+<style>
+  .about {
+    color: #42b983;
+  }
+</style>
